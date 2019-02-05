d1cd7bb3262920974c69f49b10021cca3d39dd0b
tania <tani.zhelezniak@gmail.com>
2019-02-05 15:31:24 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { Routes } from "@angular/router";
 
 import { ItemsComponent } from "./item/items.component";
 import { ItemDetailComponent } from "./item/item-detail.component";
+import {MainComponent} from "~/app/main/main.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "/items", pathMatch: "full" },
-    { path: "items", component: ItemsComponent },
-    { path: "item/:id", component: ItemDetailComponent },
+    { path: "", redirectTo: "/main", pathMatch: "full" },
+    { path: "main", component: MainComponent },
+
 ];
 
 @NgModule({
