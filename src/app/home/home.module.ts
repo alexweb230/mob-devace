513170a7c8a0bf946a26ff18b6ec4015ab67f4d5
tania <tani.zhelezniak@gmail.com>
2019-02-12 12:30:44 +0200

    step-2

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
+import {NativeScriptUISideDrawerModule} from "nativescript-ui-sidedrawer/angular";
 
 @NgModule({
   declarations: [],
   imports: [
-    NativeScriptCommonModule
+    NativeScriptCommonModule,
+      NativeScriptUISideDrawerModule
   ],
   schemas: [NO_ERRORS_SCHEMA]
 })
