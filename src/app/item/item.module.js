cb659c58dcc20f2bb07b020afac50d748e5a19c1
tania <tani.zhelezniak@gmail.com>
2019-02-12 11:09:32 +0200

    good

---
+++ src/app/item/item.module.js
@@ -0,0 +1,22 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var core_1 = require("@angular/core");
+var common_1 = require("nativescript-angular/common");
+var image_1 = require("tns-core-modules/ui/image");
+var ItemModule = /** @class */ (function () {
+    function ItemModule() {
+    }
+    ItemModule = __decorate([
+        core_1.NgModule({
+            declarations: [],
+            imports: [
+                common_1.NativeScriptCommonModule,
+                image_1.Image
+            ],
+            schemas: [core_1.NO_ERRORS_SCHEMA]
+        })
+    ], ItemModule);
+    return ItemModule;
+}());
+exports.ItemModule = ItemModule;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaXRlbS5tb2R1bGUuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpdGVtLm1vZHVsZS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHNDQUEyRDtBQUMzRCxzREFBdUU7QUFDdkUsbURBQWtEO0FBV2xEO0lBQUE7SUFBMEIsQ0FBQztJQUFkLFVBQVU7UUFSdEIsZUFBUSxDQUFDO1lBQ1IsWUFBWSxFQUFFLEVBQUU7WUFDaEIsT0FBTyxFQUFFO2dCQUNQLGlDQUF3QjtnQkFDdEIsYUFBSzthQUNSO1lBQ0QsT0FBTyxFQUFFLENBQUMsdUJBQWdCLENBQUM7U0FDNUIsQ0FBQztPQUNXLFVBQVUsQ0FBSTtJQUFELGlCQUFDO0NBQUEsQUFBM0IsSUFBMkI7QUFBZCxnQ0FBVSIsInNvdXJjZXNDb250ZW50IjpbImltcG9ydCB7IE5nTW9kdWxlLCBOT19FUlJPUlNfU0NIRU1BIH0gZnJvbSAnQGFuZ3VsYXIvY29yZSc7XG5pbXBvcnQgeyBOYXRpdmVTY3JpcHRDb21tb25Nb2R1bGUgfSBmcm9tICduYXRpdmVzY3JpcHQtYW5ndWxhci9jb21tb24nO1xuaW1wb3J0IHsgSW1hZ2UgfSBmcm9tIFwidG5zLWNvcmUtbW9kdWxlcy91aS9pbWFnZVwiO1xuXG5cbkBOZ01vZHVsZSh7XG4gIGRlY2xhcmF0aW9uczogW10sXG4gIGltcG9ydHM6IFtcbiAgICBOYXRpdmVTY3JpcHRDb21tb25Nb2R1bGUsXG4gICAgICBJbWFnZVxuICBdLFxuICBzY2hlbWFzOiBbTk9fRVJST1JTX1NDSEVNQV1cbn0pXG5leHBvcnQgY2xhc3MgSXRlbU1vZHVsZSB7IH1cbiJdfQ==
